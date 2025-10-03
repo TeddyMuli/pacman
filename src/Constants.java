7be8c5dd87d1571edaa1071d5b19e8699d8ca579
TeddyMuli <tmuli974@gmail.com>
2025-10-03 12:54:59 +0300

    add constants

---
+++ src/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static final int rowCount = 21;
+    public static final int colCount = 19;
+    public static final int tileSize = 32;
+    public static final int boardWidth = colCount * tileSize;
+    public static final int boardHeight = rowCount * tileSize;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
 import javax.swing.JFrame;
-
 public class Main {
     public static void main(String[] args) {
-        int rowCount = 21;
-        int colCount = 19;
-        int tileSize = 32;
-        int boardWidth = colCount * tileSize;
-        int boardHeight = rowCount * tileSize;
 
         JFrame frame = new JFrame("Pac Man");
         frame.setVisible(true);
-        frame.setSize(boardWidth, boardHeight);
+        frame.setSize(Constants.boardWidth, Constants.boardHeight);
         frame.setLocationRelativeTo(null); //appear at center
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
