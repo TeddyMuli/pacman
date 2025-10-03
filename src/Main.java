1cfa48f35e5c54db1e09b8d73f2f5ae28cc23282
TeddyMuli <tmuli974@gmail.com>
2025-10-03 12:39:27 +0300

    add readme

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        int rowCount = 21;
+        int colCount = 19;
+        int tileSize = 32;
+        int boardWidth = colCount * tileSize;
+        int boardHeight = rowCount * tileSize;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        JFrame frame = new JFrame("Pac Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null); //appear at center
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
